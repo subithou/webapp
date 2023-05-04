e391041a6e63a9559ff36f1166f176580cbc1d92
subithou <ousubith@gmail.com>
2023-05-04 07:14:35 +0530

    initial

--- customer/urls.py
+++ customer/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 
-from customer.views import customer_home, log_out, crop_prediction
+from customer.views import customer_home, log_out, crop_prediction, crop_prediction_result
 
 urlpatterns = [
     path('', customer_home, name='customer_home'),
     path('/crop_prediction', crop_prediction, name='customer_crop_prediction'),
+    path('/crop_prediction_result', crop_prediction_result, name='customer_crop_prediction_result'),
     path('log_out/', log_out, name='log_out'),
 
 ]
--- customer/views.py
+++ customer/views.py
@@ -2,9 +2,10 @@
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from joblib import load
+
 model = load('./savedModels/model.joblib')
 
 
@@ -15,25 +16,34 @@ def customer_home(request):
     context = {
         'name': current_user.first_name + " " + current_user.last_name
     }
-    y_pred = model.predict([[10,20,10,30,40,40,50]])
+    y_pred = model.predict([[10, 20, 10, 30, 40, 40, 50]])
     print(y_pred)
     return render(request, 'customer_home_page.html', {'context': context})
+
 
 @login_required
 def crop_prediction(request):
     if request.method == 'POST':
-        n = int(request.POST.get('n'))
-        p = int(request.POST.get('p'))
-        k = int(request.POST.get('k'))
-        rainfall = int(request.POST.get('rainfall'))
-        ph = int(request.POST.get('ph'))
-        temperature = int(request.POST.get('temperature'))
-        humidity = int(request.POST.get('humidity'))
+        n = float(request.POST.get('n'))
+        p = float(request.POST.get('p'))
+        k = float(request.POST.get('k'))
+        rainfall = float(request.POST.get('rainfall'))
+        ph = float(request.POST.get('ph'))
+        temperature = float(request.POST.get('temperature'))
+        humidity = float(request.POST.get('humidity'))
+
+        print(n, p, k, temperature, humidity, ph, rainfall)
 
         y_pred = model.predict([[n, p, k, temperature, humidity, ph, rainfall]])
-        print(y_pred)
+        print(y_pred[0])
+        return redirect('customer_crop_prediction_result', n, p, k)
 
     return render(request, 'customer_crop_prediction.html')
+
+
+def crop_prediction_result(request, n, p, k):
+    print(n, p, k)
+
 
 def log_out(request):
     logout(request)
