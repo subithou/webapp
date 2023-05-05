6560efcdcf0d25c04241398d79143f9dd78b6287
subithou <ousubith@gmail.com>
2023-05-05 06:51:01 +0530

    initial

--- customer/urls.py
+++ customer/urls.py
@@ -4,8 +4,8 @@ from customer.views import customer_home, log_out, crop_prediction, crop_predict
 
 urlpatterns = [
     path('', customer_home, name='customer_home'),
-    path('/crop_prediction', crop_prediction, name='customer_crop_prediction'),
-    path('/crop_prediction_result', crop_prediction_result, name='customer_crop_prediction_result'),
+    path('crop_prediction', crop_prediction, name='customer_crop_prediction'),
+    path('crop_prediction_result/<n>/<p>/<k>/<t>/<h>/<ph>/<r>/', crop_prediction_result, name='customer_crop_prediction_result'),
     path('log_out/', log_out, name='log_out'),
 
 ]
--- customer/views.py
+++ customer/views.py
@@ -23,6 +23,11 @@ def customer_home(request):
 
 @login_required
 def crop_prediction(request):
+    current_user = request.user
+    context = {
+        'name': current_user.first_name + " " + current_user.last_name
+    }
+
     if request.method == 'POST':
         n = float(request.POST.get('n'))
         p = float(request.POST.get('p'))
@@ -33,16 +38,34 @@ def crop_prediction(request):
         humidity = float(request.POST.get('humidity'))
 
         print(n, p, k, temperature, humidity, ph, rainfall)
+        return redirect('customer_crop_prediction_result', n, p, k, temperature, humidity, ph, rainfall)
 
-        y_pred = model.predict([[n, p, k, temperature, humidity, ph, rainfall]])
-        print(y_pred[0])
-        return redirect('customer_crop_prediction_result', n, p, k)
-
-    return render(request, 'customer_crop_prediction.html')
+    return render(request, 'customer_crop_prediction.html', {'context': context})
 
 
-def crop_prediction_result(request, n, p, k):
-    print(n, p, k)
+@login_required
+def crop_prediction_result(request, n, p, k, t,h,ph,r ):
+    current_user = request.user
+
+
+    temperature = t
+    humidity = h
+    rainfall = r
+
+    y_pred = model.predict([[float(n), float(p), float(k), float(temperature), float(humidity), float(ph), float(rainfall)]])
+    print(y_pred[0])
+    context = {
+        'crop': y_pred[0].upper(),
+        'n': n,
+        'p': p,
+        'k': k,
+        'temperature': temperature,
+        'rainfall': rainfall,
+        'humidity': humidity,
+        'ph': ph,
+        'name': current_user.first_name + " " + current_user.last_name
+    }
+    return render(request, 'customer_crop_prediction_result.html', {'context': context})
 
 
 def log_out(request):
